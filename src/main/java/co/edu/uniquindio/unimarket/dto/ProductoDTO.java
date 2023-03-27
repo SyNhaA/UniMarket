dc1dd0b1c2d612c6ec5f790d19a3f00f788b578c
SyNhaA <juans.rodriguezp@uqvirtual.edu.co>
2023-03-26 20:08:52 -0500

    Se crearon los paquetes

---
+++ src/main/java/co/edu/uniquindio/unimarket/dto/ProductoDTO.java
@@ -0,0 +1,29 @@
+package co.edu.uniquindio.unimarket.dto;
+
+import co.edu.uniquindio.unimarket.modelo.Categoria;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class ProductoDTO {
+
+    private String nombre;
+
+    private String descripcion;
+
+    private int unidades;
+
+    private float precio;
+
+    private int codigoVendedor;
+
+    private List<String> imagenes;
+
+    private List<Categoria> categorias;
+
+}
